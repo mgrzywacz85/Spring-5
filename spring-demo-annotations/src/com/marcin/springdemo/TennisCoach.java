333b5d2cbfad5428daccda240ab6bb610e7105d8
unknown <professor.marcin@gmail.com>
2021-10-19 20:00:58 +0200

    Dependency injection directly on field - @Autowired

--- spring-demo-annotations/src/com/marcin/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/marcin/springdemo/TennisCoach.java
@@ -6,14 +6,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 	
+	//dependency injection directly on the field using @Autowired below - via Java Reflection
+	
+	@Autowired
 	private FortuneService fortuneService;
 	
 	//dependency injection via any method below as long as @Autowired tag is used
 	
-	@Autowired
-	public void randomMethodName(FortuneService fortuneService) {
-		this.fortuneService = fortuneService;
-	}
+//	@Autowired
+//	public void randomMethodName(FortuneService fortuneService) {
+//		this.fortuneService = fortuneService;
+//	}
 		
 	//dependency injection via constructor below
 
