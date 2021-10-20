8537d6e245f0dd3b41232b3fb1b0106aec7ff1b9
unknown <professor.marcin@gmail.com>
2021-10-20 14:07:27 +0200

    TennisCoach scope changed to prototype

--- spring-demo-annotations/src/com/marcin/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/marcin/springdemo/TennisCoach.java
@@ -2,32 +2,16 @@ package com.marcin.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
 	
-//	//dependency injection directly on the field using @Autowired below - via Java Reflection
-//	
-//	//@Qualifier defines which implementation of the interface to inject
-//	
-//	@Autowired
-//	@Qualifier("averageFortuneService")
-	private FortuneService fortuneService;
-//	
-//	//dependency injection via any method below as long as @Autowired tag is used
-	
-//	@Autowired
-//	public void randomMethodName(FortuneService fortuneService) {
-//		this.fortuneService = fortuneService;
-//	}
-		
-	//dependency injection via constructor below
-
 	@Autowired
-	public TennisCoach(@Qualifier("averageFortuneService") FortuneService fortuneService) {
-		this.fortuneService = fortuneService;
-	}
+	@Qualifier("averageFortuneService")
+	private FortuneService fortuneService;
 	
 	@Override
 	public String getDailyWorkout() {
