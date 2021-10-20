166f82579772b80ed8da3930911514be369f284f
unknown <professor.marcin@gmail.com>
2021-10-20 14:14:56 +0200

    @PostConstruct and @PreDestroy lifecycle annotations

--- spring-demo-annotations/src/com/marcin/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/marcin/springdemo/TennisCoach.java
@@ -1,13 +1,30 @@
 package com.marcin.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+//@Scope("prototype")
 public class TennisCoach implements Coach {
+	
+	//define init method - can be any name, does not take args and usually void as result cannot be captured
+	
+	@PostConstruct
+	public void postInitMethod() {
+		System.out.println("Starting...");
+	}	
+	
+	//define destroy method - can be any name, does not take args and usually void as result cannot be captured
+	
+	@PreDestroy
+	public void preDestroyMethod() {
+		System.out.println("Finishing...");
+	}
 	
 	@Autowired
 	@Qualifier("averageFortuneService")
