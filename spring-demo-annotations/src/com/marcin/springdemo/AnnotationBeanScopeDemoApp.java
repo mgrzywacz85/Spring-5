9da1a2744d41f249f777d2f11eddcb9bda4a4ca0
unknown <professor.marcin@gmail.com>
2021-10-20 14:01:42 +0200

    Bean scope with annotations

---
+++ spring-demo-annotations/src/com/marcin/springdemo/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,28 @@
+package com.marcin.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		//load spring config file
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//retrieve bean from container
+		
+		Coach firstCoach = context.getBean("tennisCoach", Coach.class);
+		
+		Coach secondCoach = context.getBean("tennisCoach", Coach.class);
+		
+		boolean result = firstCoach == secondCoach;
+		
+		System.out.println(result);
+		
+		
+		
+		
+		
+	}
+
+}
