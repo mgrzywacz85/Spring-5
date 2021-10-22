9c6b5408cd5fc57a22276a641e0d33830c72842c
unknown <professor.marcin@gmail.com>
2021-10-22 14:12:16 +0200

    Reading Spring config from Java class

---
+++ spring-demo-annotations/src/com/marcin/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,34 @@
+package com.marcin.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+
+		//read spring config java class
+		
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+				
+		
+		//get bean from spring container
+		
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		
+		
+		//call methods on the bean		
+		
+		System.out.println(theCoach.getDailyWorkout());
+		
+		//call method on the dependency - HappyFortuneService
+		
+		System.out.println(theCoach.getDailyFortune());
+		
+		//close the container
+		
+		context.close();
+		
+		
+	}
+
+}
---
+++ spring-demo-annotations/src/com/marcin/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.marcin.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.marcin.springdemo")
+public class SportConfig {
+
+}
